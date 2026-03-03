483df08f4aa0648fd8d2cf7f68881d4c3c281595
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-03-03 22:18:15 +0100

    avance

--- src/empresa_paqueteria/Paquete.java
+++ src/empresa_paqueteria/Paquete.java
@@ -2,8 +2,57 @@ package empresa_paqueteria;
 
 public class Paquete {
 
+    private static int contador = 1;
     private int numeroDeSeguimiento;
     private String remitente;
     private String destinatario;
     private int prioridad;
+
+    public Paquete(String remitente, String destinatario, int prioridad) {
+        contador++;
+        this.numeroDeSeguimiento = contador;
+        this.remitente = remitente;
+        this.destinatario = destinatario;
+        this.prioridad = prioridad;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public static void setContador(int contador) {
+        Paquete.contador = contador;
+    }
+
+    public int getNumeroDeSeguimiento() {
+        return numeroDeSeguimiento;
+    }
+
+    public void setNumeroDeSeguimiento(int numeroDeSeguimiento) {
+        this.numeroDeSeguimiento = numeroDeSeguimiento;
+    }
+
+    public String getRemitente() {
+        return remitente;
+    }
+
+    public void setRemitente(String remitente) {
+        this.remitente = remitente;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
 }
--- src/empresa_paqueteria/Ruta.java
+++ src/empresa_paqueteria/Ruta.java
@@ -1,7 +1,15 @@
 package empresa_paqueteria;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Ruta {
 
     private String nombre;
+    private Deque<Paquete> paquetes;
 
+    public Ruta(String nombre) {
+        this.nombre = nombre;
+        this.paquetes = new ArrayDeque<>();
+    }
 }
